7102bf867d69b8538430f233ccd2beafc100b292
efwandha yudhono <efwandha231@gmail.com>
2021-06-17 00:32:49 +0700

    Multipe Flows

--- app/src/main/java/org/d3if0032/assessment2/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/org/d3if0032/assessment2/ui/tasks/TasksViewModel.kt
@@ -4,6 +4,7 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import org.d3if0032.assessment2.data.TaskDao
 
@@ -12,11 +13,22 @@ class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
     ) : ViewModel() {
 
-        val  searchQuery = MutableStateFlow("")
+        val searchQuery = MutableStateFlow("")
 
-        private val tasksFlow = searchQuery.flatMapLatest {
-            taskDao.getTasks(it)
+        val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+        val hideCompleted = MutableStateFlow(false)
+
+        private val tasksFlow = combine(
+            searchQuery,
+            sortOrder,
+            hideCompleted
+        ){query, sortOrder, hideCompleted ->
+            Triple(query, sortOrder, hideCompleted)
+        }
+            .flatMapLatest { (query, sortOrder, hidecompleted) ->
+            taskDao.getTasks(query, sortOrder, hidecompleted)
         }
 
         val tasks = tasksFlow.asLiveData()
 }
+enum class SortOrder{ BY_NAME, BY_DATE}
